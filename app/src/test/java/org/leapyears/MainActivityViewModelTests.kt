a604e4c6a1a4ac48c3f74dd156d8382f3a7a3ac9
2019-DEV-120 <philippe.simons@movify.com>
2019-02-28 12:52:13 +0100

    unit test MainActivityViewModel class

---
+++ app/src/test/java/org/leapyears/MainActivityViewModelTests.kt
@@ -0,0 +1,24 @@
+package org.leapyears
+
+import androidx.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(InstantExecutorExtension::class)
+class MainActivityViewModelTests {
+
+    @Test
+    fun `checkYear updates isLeapYear`() {
+        // given
+        val viewModel = MainActivityViewModel()
+        val observer = mock<Observer<Boolean>>()
+        viewModel.isLeapYear.observeForever(observer)
+        // when
+        viewModel.checkYear("2000")
+        // then
+        verify(observer).onChanged(any())
+    }
+}
